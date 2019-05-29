42a2db04483d43973680099d89a28afa2edac358
vitalii <dumavb777@gmail.com>
2019-05-29 11:43:44 +0300

    sol

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const containerCounter = document.querySelector('span');
 containerCounter.textContent = sessionStorage.getItem('currentDigit') || 0;
 
 document.addEventListener('click', () => {
-    containerCounter.textContent = (+containerCounter.textContent) + 1;
-    sessionStorage.setItem('currentDigit', containerCounter.textContent);
+    const counter = (+containerCounter.textContent) + 1;
+    containerCounter.textContent = counter;
+    sessionStorage.setItem('currentDigit', counter);
 })
